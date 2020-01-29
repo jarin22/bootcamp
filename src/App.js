abd3b3c621dc2a396eaf1f244d9117a89f2508b6
Karjala <Jarin.Karjala@us.sogeti.com>
2020-01-29 14:22:45 -0500

    connect with employee

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props){
+    super(props)
+
+    this.state={
+      employees: []
+    }
+  }
+
+componentDidMount(){
+  fetch('http://localhost:3005/')
+  .then(employees => employees.json())
+  .then(employees => {
+    console.log(employees)
+    this.setState({
+        employees:employees
+    })
+  })
+  .catch(err => console.log(err)) 
+
+}
+
+
+
+
+  render(){ 
+    return (
+      <div className="App container">
+        <h1>Sogeti Employee Directory</h1>
+          <div className="row">
+          {this.state.employees.map((employee, index) => {
+            return (
+              <div class="media col-12 mb-4 p-3 directory-entry">
+              <img src={employee.img} class="mr-3 directory-image" alt="..."/>
+              <div class="media-body">
+                <h5 class="mt-0">Name: - {employee.name}</h5>
+                <h6 class="mt-0">Location: - {employee.location}
+                <p class="directory-phone">Phone: - {employee.phone} </p>
+                </h6>
+              </div>
+            </div>
+
+            
+          )
+        }) 
+      }
+      </div>
+      
+      </div>
+    );
+  }
+  
 }
 
 export default App;
